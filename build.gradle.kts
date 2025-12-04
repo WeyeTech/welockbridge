ee66c264a754d75d28fe1195c22f44ab76d3eee5
“Mrityunjayri” <mrityunjay.patel@wheelseye.com>
2025-12-04 23:24:32 +0530

    final

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,8 +64,8 @@ configure<com.android.build.gradle.LibraryExtension> {
   }
   
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
   
   buildFeatures {
