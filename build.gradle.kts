fd6adeca6a82537d9d492fd60ad8dc4ad272623d
“Mrityunjayri” <mrityunjay.patel@wheelseye.com>
2025-12-04 12:27:21 +0530

    final fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 // SDK Version Configuration  (JitPack coordinates)
 // ============================================================================
 
-val sdkVersion = "1.1.1"
+val sdkVersion = "1.1.2"
 val sdkGroupId = "com.github.WeyeTech"
 val sdkArtifactId = "welockbridge"
 
